f5a3c4b2f96bb03a0b4466b26b0f0ff0fb45c8ee
madcyn <35504047+madcyn@users.noreply.github.com>
2021-03-12 12:15:16 -0800

    ffff

--- FMMVC/Startup.cs
+++ FMMVC/Startup.cs
@@ -1,3 +1,4 @@
+
 using FMBL;
 using FMDL;
 using Microsoft.AspNetCore.Builder;
@@ -33,7 +34,7 @@ namespace FMMVC
                 options.Cookie.HttpOnly = true;
             });
             services.AddScoped<IFluffyRepo, FluffyRepo>();
-            services.AddScoped<IFluffyBL, FluffyBL>();
+           services.AddScoped<IFluffyBL, FluffyBL>();
             services.AddControllersWithViews();
         }
 
