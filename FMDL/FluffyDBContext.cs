181241ddb4020ebe62dadf452f4554cb41602acf
madcyn <35504047+madcyn@users.noreply.github.com>
2021-03-12 09:25:11 -0800

    my stuff

--- FMDL/FluffyDBContext.cs
+++ FMDL/FluffyDBContext.cs
@@ -16,6 +16,7 @@ namespace FMDL
         {
         }
         //declare to EF Core that these are the models I want to be persisted to my db
-        public DbSet<Element> 
+        //  public DbSet<Element>{get;set };
+
     }
 }
